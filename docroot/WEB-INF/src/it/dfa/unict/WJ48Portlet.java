6fdd468afe22e57b6494bf3692b53942cfb1a6ac
mtorrisi <torrisi.mario@gmail.com>
2017-02-08 14:39:55 +0100

    Implement job submission through Futuregateway

--- docroot/WEB-INF/src/it/dfa/unict/WJ48Portlet.java
+++ docroot/WEB-INF/src/it/dfa/unict/WJ48Portlet.java
@@ -1,20 +1,42 @@
 package it.dfa.unict;
 
 import it.dfa.unict.pojo.AppInput;
+import it.dfa.unict.pojo.InputFile;
+import it.dfa.unict.pojo.Link;
+import it.dfa.unict.pojo.Task;
 import it.dfa.unict.util.Constants;
+import it.dfa.unict.util.Utils;
 
+import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
+import javax.portlet.PortletConfig;
 import javax.portlet.PortletException;
+import javax.portlet.PortletPreferences;
 import javax.portlet.ProcessAction;
-
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.LiferayPortletConfig;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.servlet.SessionMessages;
+import com.liferay.portal.kernel.upload.UploadPortletRequest;
+import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.JavaConstants;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.model.User;
+import com.liferay.portal.theme.ThemeDisplay;
+import com.liferay.portal.util.PortalUtil;
 import com.liferay.util.bridges.mvc.MVCPortlet;
+import com.sun.jersey.api.client.UniformInterfaceException;
 
 public class WJ48Portlet extends MVCPortlet {
 
@@ -48,80 +70,128 @@ public class WJ48Portlet extends MVCPortlet {
 	public void submit(ActionRequest actionRequest,
 			ActionResponse actionResponse) throws IOException, PortletException {
 
-//		AppInput appInput = new AppInput();
-//		PortletPreferences preferences = actionRequest.getPreferences();
-//
-//		String JSONAppPrefs = GetterUtil.getString(preferences.getValue(
-//				Constants.APP_PREFERENCES, null));
-//		AppPreferences appPrefs = Utils.getAppPreferences(JSONAppPrefs);
-//
-//		SimpleDateFormat dateFormat = new SimpleDateFormat(Constants.TS_FORMAT);
-//		String timestamp = dateFormat.format(Calendar.getInstance().getTime());
-//		appInput.setTimestamp(timestamp);
-//
-//		ThemeDisplay themeDisplay = (ThemeDisplay) actionRequest
-//				.getAttribute(WebKeys.THEME_DISPLAY);
-//		User user = themeDisplay.getUser();
-//		String username = user.getScreenName();
-//		appInput.setUsername(username);
-//		UploadPortletRequest uploadRequest = PortalUtil
-//				.getUploadPortletRequest(actionRequest);
-//
-//		File uploadedFile = processInputFile(uploadRequest, username,
-//				timestamp, appInput);
-//
-//		if (uploadedFile != null && uploadedFile.length() == 0) {
-//			SessionErrors.add(actionRequest, "empty-file");
-//		} else {
-//
-//			if (uploadedFile != null)
-//				appInput.setInputSandbox(uploadedFile.getAbsolutePath());
-//
-//			String joblabel = ParamUtil.getString(uploadRequest, "jobLabel");
-//
-//			appInput.setJobLabel(joblabel);
-//
-//			_log.info(appInput);
-//
-//			// List<AppInfrastructureInfo> enabledInfras = Utils
-//			// .getEnabledInfrastructureInfo(JSONAppInfras);
-//			//
-//			// if (enabledInfras.size() > 0) {
-//			// InfrastructureInfo infrastructureInfo[] = Utils
-//			// .convertAppInfrastructureInfo(enabledInfras);
-//			//
-//			// submitJob(appPrefs, appInput, infrastructureInfo);
-//			//
-//			// PortalUtil.copyRequestParameters(actionRequest, actionResponse);
-//			// actionResponse.setRenderParameter("jobLabel", joblabel);
-//			// actionResponse
-//			// .setRenderParameter("jspPage", "/jsps/submit.jsp");
-//			// }
-//		}
-//
-//		// Hide default Liferay success/error messages
-//		PortletConfig portletConfig = (PortletConfig) actionRequest
-//				.getAttribute(JavaConstants.JAVAX_PORTLET_CONFIG);
-//		LiferayPortletConfig liferayPortletConfig = (LiferayPortletConfig) portletConfig;
-//		SessionMessages.add(actionRequest, liferayPortletConfig.getPortletId()
-//				+ SessionMessages.KEY_SUFFIX_HIDE_DEFAULT_ERROR_MESSAGE);
-		throw new NotImplementedException();
-	}
+		ThemeDisplay themeDisplay = (ThemeDisplay) actionRequest
+				.getAttribute(WebKeys.THEME_DISPLAY);
+		User user = themeDisplay.getUser();
+		String username = user.getScreenName();
 
-	/**
-	 * Method responsible of job submission.
-	 * 
-	 * @param preferences
-	 *            Application preferences object.
-	 * @param appInput
-	 *            An object consists of view input fields values and other job
-	 *            input parameters.
-	 * @param enabledInfrastructures
-	 *            List of enabled infrastructures.
-	 */
-	private void submitJob(AppPreferences preferences, AppInput appInput) {
+		UploadPortletRequest uploadRequest = PortalUtil
+				.getUploadPortletRequest(actionRequest);
+		SimpleDateFormat dateFormat = new SimpleDateFormat(Constants.TS_FORMAT);
+		String timestamp = dateFormat.format(Calendar.getInstance().getTime());
+		try {
 
-		_log.info("To be implemented by future gateways APIs");
+			File uploadedFile = processInputFile(uploadRequest, username,
+					timestamp);
+			if (uploadedFile != null && uploadedFile.length() == 0) {
+				_log.warn("Uploaded an empity file.");
+				SessionErrors.add(actionRequest, "empty-file");
+			} else {
+				// Path to the uploaded file in the Liferay server
+				String filePath = uploadedFile.getAbsolutePath();
+
+				_log.debug("PATH: " + filePath);
+
+				PortletPreferences preferences = actionRequest.getPreferences();
+				String JSONAppPrefs = GetterUtil.getString(preferences
+						.getValue(Constants.APP_PREFERENCES, null));
+				_log.info(JSONAppPrefs);
+				AppPreferences appPrefs = Utils.getAppPreferences(JSONAppPrefs);
+				_log.debug(appPrefs);
+
+				if (appPrefs.getApplicationId() <= 0) {
+					SessionErrors.add(actionRequest, "wrong-app-id");
+				} else if (Validator.isIPAddress(appPrefs.getFgHost())
+						|| Validator.isHostName(appPrefs.getFgHost())) {
+
+					AppInput appInput = new AppInput();
+					appInput.setApplication(appPrefs.getApplicationId());
+
+					// Just a description of the job, could passed from the JSP
+					// maybe.
+					appInput.setDescription("WEKA J48 Application");
+
+					String[] inputSandbox = null;
+					File inputARFF = new File(filePath);
+					List<InputFile> inputFiles = new ArrayList<InputFile>();
+
+					InputFile inputFile = new InputFile();
+					inputFile.setName(inputARFF.getName());
+					inputFiles.add(inputFile);
+
+					inputSandbox = new String[] { filePath };
+					appInput.setInputFiles(inputFiles);
+
+					appInput.getArguments().add(inputARFF.getName());
+
+					String classifier = uploadRequest.getParameter("clasify");
+					appInput.getArguments().add(classifier);
+
+					_log.info(appInput);
+					FutureGatewayClient client = new FutureGatewayClient(
+							appPrefs.getFgHost(), appPrefs.getFgPort(),
+							appPrefs.getFgAPIVersion());
+
+					// 1. Create FG task
+					try {
+						Task t = client.createTask(appInput, username);
+						_log.info(t);
+						if (t.getStatus().equals("WAITING")
+								&& inputSandbox != null) {
+							// 2. upload input file
+							String uploadPath = "";
+							List<Link> links = t.getLinks();
+							for (Link link : links) {
+								if (link.getRel().equals("input")) {
+									uploadPath = link.getHref();
+									break;
+								}
+							}
+							String t2 = client.uploadFile(uploadPath,
+									inputSandbox);
+							_log.info(t2);
+							// TODO Check the FG response to see if the file was
+							// correctly uploaded --> "gestatus": "triggered" in
+							// the
+							// respose notify user that task was correctly
+							// submitted and
+							// he can check status on my-jobs page
+						} else {
+							// TODO Manage this condition
+						}
+					} catch (UniformInterfaceException | IOException e) {
+						_log.error(e.getMessage());
+						SessionErrors.add(actionRequest, "error");
+						actionResponse.setRenderParameter("jspPage",
+								"/jsps/view.jsp");
+					}
+
+					actionResponse.setRenderParameter("jspPage",
+							"/jsps/submit.jsp");
+				} else {
+					SessionErrors.add(actionRequest, "wrong-fg-host");
+				}
+				// Hide default Liferay success/error messages
+				PortletConfig portletConfig = (PortletConfig) actionRequest
+						.getAttribute(JavaConstants.JAVAX_PORTLET_CONFIG);
+				LiferayPortletConfig liferayPortletConfig =
+						(LiferayPortletConfig) portletConfig;
+				SessionMessages.add(actionRequest,
+						liferayPortletConfig.getPortletId()
+						+ SessionMessages.KEY_SUFFIX_HIDE_DEFAULT_ERROR_MESSAGE);
+
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// Hide default Liferay success/error messages
+		PortletConfig portletConfig = (PortletConfig) actionRequest
+				.getAttribute(JavaConstants.JAVAX_PORTLET_CONFIG);
+		LiferayPortletConfig liferayPortletConfig = (LiferayPortletConfig) portletConfig;
+		SessionMessages.add(actionRequest, liferayPortletConfig.getPortletId()
+				+ SessionMessages.KEY_SUFFIX_HIDE_DEFAULT_ERROR_MESSAGE);
 
 	}
 
@@ -129,40 +199,36 @@ public class WJ48Portlet extends MVCPortlet {
 	 * @param uploadRequest
 	 * @param username
 	 * @param timestamp
-	 * @param appInput
 	 * @return
 	 * @throws IOException
 	 */
-//	private File processInputFile(UploadPortletRequest uploadRequest,
-//			String username, String timestamp, AppInput appInput)
-//			throws IOException {
-//
-//		File file = null;
-//		String fileInputName = "fileupload";
-//
-//		String sourceFileName = uploadRequest.getFileName(fileInputName);
-//
-//		if (Validator.isNotNull(sourceFileName)) {
-//			_log.debug("Uploading file: " + sourceFileName + " ...");
-//
-//			String fileName = FileUtil.stripExtension(sourceFileName);
-//			_log.debug(fileName);
-//
-//			appInput.setInputFileName(fileName);
-//
-//			String extension = FileUtil.getExtension(sourceFileName);
-//			_log.debug(extension);
-//
-//			// Get the uploaded file as a file.
-//			File uploadedFile = uploadRequest.getFile(fileInputName, true);
-//			File folder = new File(Constants.ROOT_FOLDER_NAME);
-//			// This is our final file path.
-//			file = new File(folder.getAbsolutePath() + Constants.FILE_SEPARATOR
-//					+ username + "_" + timestamp + "_" + fileName
-//					+ ((!extension.isEmpty()) ? "." + extension : ""));
-//			FileUtil.move(uploadedFile, file);
-//
-//		}
-//		return file;
-//	}
+	private File processInputFile(UploadPortletRequest uploadRequest,
+			String username, String timestamp) throws IOException {
+
+		File file = null;
+		String fileInputName = "uploadDataset";
+
+		String sourceFileName = uploadRequest.getFileName(fileInputName);
+
+		if (Validator.isNotNull(sourceFileName)) {
+			_log.debug("Uploading file: " + sourceFileName + " ...");
+
+			String fileName = FileUtil.stripExtension(sourceFileName);
+			_log.debug(fileName);
+
+			String extension = FileUtil.getExtension(sourceFileName);
+			_log.debug(extension);
+
+			// Get the uploaded file as a file.
+			File uploadedFile = uploadRequest.getFile(fileInputName, true);
+			File folder = new File(Constants.ROOT_FOLDER_NAME);
+			// This is our final file path.
+			file = new File(folder.getAbsolutePath() + Constants.FILE_SEPARATOR
+					+ username + "_" + timestamp + "_" + fileName
+					+ ((!extension.isEmpty()) ? "." + extension : ""));
+			FileUtil.move(uploadedFile, file);
+
+		}
+		return file;
+	}
 }
--- docroot/jsps/view.jsp
+++ docroot/jsps/view.jsp
@@ -9,20 +9,25 @@
 <%@ taglib uri="http://alloy.liferay.com/tld/aui" prefix="aui"%>
 <%@ taglib uri="http://liferay.com/tld/ui" prefix="liferay-ui"%>
 
+<liferay-ui:error key="wrong-app-id" message="wrong-app-id" />
+<liferay-ui:error key="wrong-fg-host" message="wrong-fg-host" />
+
 <aui:column columnWidth="50" first="true">
-		<img src="<%=request.getContextPath()%>/images/J48.jpg"
-			height="80%" width="80%" />
+	<img src="<%=request.getContextPath()%>/images/J48.jpg" height="80%"
+		width="80%" />
 </aui:column>
 
 <portlet:defineObjects />
 <h2>WEKA - J48 Algorithm</h2>
 <p>
-	This portlet will help to analyse your uploaded data with C4.5 algorithm using J48, WEKA's implementation of decision tree learner.
-        <aui:a href="http://www.cs.waikato.ac.nz/ml/weka/" label="WEKA"></aui:a>
-	is a state-of-the-art facility for developing machine learning techniques 
-        and their application to real-world data mining problems. It is a collection 
-        of machine learning algorithms for data mining tasks. This portlet will, however,
-        only make use of a particular classification algorithm to analyse data sets.
+	This portlet will help to analyse your uploaded data with C4.5
+	algorithm using J48, WEKA's implementation of decision tree learner.
+	<aui:a href="http://www.cs.waikato.ac.nz/ml/weka/" label="WEKA"></aui:a>
+	is a state-of-the-art facility for developing machine learning
+	techniques and their application to real-world data mining problems. It
+	is a collection of machine learning algorithms for data mining tasks.
+	This portlet will, however, only make use of a particular
+	classification algorithm to analyse data sets.
 <h3>Upload your Dataset</h3>
 <hr />
 <p></p>
@@ -30,41 +35,38 @@
 <portlet:actionURL name="submit" var="uploadFileURL" />
 <aui:form action="<%=uploadFileURL%>" method="post"
 	enctype="multipart/form-data">
-    
+	<liferay-ui:error key="empty-file" message="empty-file" />
+	<aui:input name="uploadDataset" title="Dataset upload" type="file"
+		label="Select Dataset:" />
 
-	<aui:input name="upload dataset" title="Dataset upload" type="file" />
+	<hr />
 
-	<!-- <aui:input name="url" title="URL" type="text" /> -->
-        
-        <hr />
-        
-        <h3>Choose classifier and select test option</h3>
+	<h3>Choose classifier and select test option</h3>
 
-	<p>Once you have your data set loaded, select and apply the appropriate 
-           classifier and set test option below. For the purpose this portlet, 
-           you'll analyse the data with C4.5 algorithm using J48. 
-           In addition, the "Percentage split" test option will be chosen 
-           and it will predict about 66% of the tested data. 
-	</p>
-        
-        <h3>Classify</h3>
+	<p>Once you have your data set loaded, select and apply the
+		appropriate classifier and set test option below. For the purpose this
+		portlet, you'll analyse the data with C4.5 algorithm using J48. In
+		addition, the "Percentage split" test option will be chosen and it
+		will predict about 66% of the tested data.</p>
+
+	<h3>Classify</h3>
 	<hr />
 	<aui:select label="Select classifier" name="clasify">
-		<aui:option label="J48" value="j48"></aui:option>
+		<aui:option label="J48" value="weka.classifiers.trees.J48" />
 	</aui:select>
-        
-        <h3>Test options</h3>
+
+	<h3>Test options</h3>
 	<hr />
 
 	<aui:select label="Select test option" name="test">
-		<aui:option label="Percentage split" value="percentageSplit"></aui:option>
+		<aui:option label="Percentage split" value="percentageSplit" />
 	</aui:select>
-        
-        <br />
 
-	<aui:button type="submit" value="submit"></aui:button>
-	
-	
+	<br />
+
+	<aui:button type="submit" value="submit" />
+
+
 
 </aui:form>
 <!-- 
